15d922a55d43ca1fe9c24b9e5789edd2288b2f06
Jrsouza8315 <hbrcomercialssa@gmail.com>
2022-03-06 10:26:34 -0300

    config

---
+++ jogo-RPG-C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using 
+using jogo_RPG_C#.src.Entities
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Arus hero = new Arus("Arus", 23, "Knight");
+            
+        Console.WriteLine("");
+    }
+
+}
---
+++ jogo-RPG-C#/src/Entities/Arus.cs
@@ -0,0 +1,22 @@
+namespace jogo_RPG_C#.src.Entities
+{
+    public class Arus
+    {
+
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = level;
+            this.HeroType = HeroType;
+        }
+
+        public Arus (){
+
+        }
+
+        public string Name; 
+        public int level;
+        public string HeroType;
+        
+    }
+}
